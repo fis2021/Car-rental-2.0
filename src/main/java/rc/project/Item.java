f980fefc2780716a5a30ff21bff989b4429e7002
Radu Serban <r.serbannn@gmail.com>
2021-05-15 18:46:00 +0300

    [CR-74] Show message if the dates aren't selected before confirmation

--- src/main/java/rc/project/Item.java
+++ src/main/java/rc/project/Item.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -50,6 +51,15 @@ public class Item {
     @FXML
     private Label priceText;
 
+    @FXML
+    private DatePicker dateFrom;
+
+    @FXML
+    private DatePicker dateTo;
+
+    @FXML
+    private Label warningLabel;
+
     private Car car;
 
     public void setData(Car car) {
@@ -63,7 +73,7 @@ public class Item {
         pane.setStyle("-fx-background-color: radial-gradient(center 50% 50% , radius 60% ,#" + car.getColor1() +", #" + car.getColor2() +");");
     }
 
-    private int k = 1, ok = 1;
+    private int i = 1, k = 1, ok = 1;
     public void loadDetalii(ActionEvent event) throws IOException {
         if(k==1 && ok == 1) {
             this.itemPhoto.setVisible(false);
@@ -84,42 +94,53 @@ public class Item {
             this.itemDetails.setText("Details");
             k = 1;
         }
+        if(i > 1 && (dateFrom.getValue() != null) && (dateTo.getValue() != null)) {
+            System.out.println("Confirm");
+            this.warningLabel.setVisible(false);
+        } else if((dateFrom.getValue() == null) || (dateTo.getValue() == null)){
+            this.warningLabel.setVisible(true);
+            this.warningLabel.setText("Please complete the dates");
+        } else {
+            this.warningLabel.setVisible(false);
+        }
     }
 
-    private int i = 1;
     public void loadOrders(ActionEvent event) throws IOException {
-            if (i == 1) {
-                ok = 0;
-                this.itemPhoto.setVisible(false);
-                this.itemText.setVisible(false);
-                this.itemPrice.setVisible(false);
-                this.ordersVbox.setVisible(true);
-                this.itemOrder.setText("Show price");
-                this.itemDetails.setText("Confirm");
-                i++;
-            } else if (i == 2) {
-                this.itemPhoto.setVisible(false);
-                this.itemText.setVisible(false);
-                this.itemPrice.setVisible(false);
-                this.ordersVbox.setVisible(true);
-                this.itemOrder.setText("Go back");
-                this.itemDetails.setText("Confirm");
-                this.totalPrice.setVisible(true);
-                this.priceText.setVisible(true);
-                i++;
-            } else if (i == 3) {
-                this.ordersVbox.setVisible(false);
-                this.itemPhoto.setVisible(true);
-                this.itemDetails.setVisible(true);
-                this.itemOrder.setVisible(true);
-                this.itemText.setVisible(true);
-                this.itemPrice.setVisible(true);
-                this.totalPrice.setVisible(false);
-                this.priceText.setVisible(false);
-                this.itemOrder.setText("Order");
-                this.itemDetails.setText("Details");
-                ok = 1;
-                i = 1;
-            }
+        if (i == 1) {
+            ok = 0;
+            this.itemPhoto.setVisible(false);
+            this.itemText.setVisible(false);
+            this.itemPrice.setVisible(false);
+            this.ordersVbox.setVisible(true);
+            this.warningLabel.setVisible(false);
+            this.itemOrder.setText("Show price");
+            this.itemDetails.setText("Confirm");
+            i++;
+        } else if (i == 2) {
+            this.itemPhoto.setVisible(false);
+            this.itemText.setVisible(false);
+            this.itemPrice.setVisible(false);
+            this.ordersVbox.setVisible(true);
+            this.warningLabel.setVisible(false);
+            this.itemOrder.setText("Go back");
+            this.itemDetails.setText("Confirm");
+            this.totalPrice.setVisible(true);
+            this.priceText.setVisible(true);
+            i++;
+        } else if (i == 3) {
+            this.ordersVbox.setVisible(false);
+            this.itemPhoto.setVisible(true);
+            this.itemDetails.setVisible(true);
+            this.itemOrder.setVisible(true);
+            this.itemText.setVisible(true);
+            this.itemPrice.setVisible(true);
+            this.totalPrice.setVisible(false);
+            this.priceText.setVisible(false);
+            this.warningLabel.setVisible(false);
+            this.itemOrder.setText("Order");
+            this.itemDetails.setText("Details");
+            ok = 1;
+            i = 1;
+        }
     }
 }
